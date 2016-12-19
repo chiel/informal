3aa281311c9c5f33f2d67526ad73194171bde471
Andre Fonseca <mail@andrefonseca.pt>
2016-12-19 14:04:41 +0100

    Add option for a text field to subscribe others
    This may break some features, revert if needed

--- src/fields/text.js
+++ src/fields/text.js
@@ -7,9 +7,10 @@ var Base = require('./base');
  * @param {Object} spec
  * @param {String} value
  */
-var Text = function(spec, value){
+var Text = function(spec, value, subValues){
 	if (!(this instanceof Text)) return new Text(spec, value);
 	Base.call(this, spec, value);
+	this.subValues = subValues;
 };
 
 require('inherits')(Text, Base);
@@ -22,5 +23,20 @@ Text.prototype.build = function(){
 	this.input.setAttribute('type', this.spec.type);
 	this.input.value = this.value || this.spec.value || '';
 };
+
+Text.prototype.notify = function(field, value) {
+	if(!this.spec.subscribe) return;
+	var subscriptions = typeof this.spec.subscribe === 'string' ? [this.spec.subscribe] : this.spec.subscribe;
+	var subValues = this.subValues;
+	var glue = this.spec['joinWith'] || ' ';
+	var reg = new RegExp('^(' + glue + ')+$');
+	var old = subscriptions.map( function(sub){ return subValues[sub] } ).join(glue);
+
+	if(this.input.value !== old.replace(reg, '')) return;
+
+	subValues[field] = value;
+
+	this.input.value = subscriptions.map( function(sub){ return subValues[sub] } ).join(glue);
+}
 
 module.exports = Text;
